add9b8796e58324ce9bfa9c1fd33b3064afbbf3c
vipinnagar <vnagar@iitk.ac..in>
2021-08-03 16:58:25 +0530

    complete Resume

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'djangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Added Manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
